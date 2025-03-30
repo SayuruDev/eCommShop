e786d7b614e3e8c26d4e485d3a2d814b0e6d827f
SayuruDev <mailtosayuru@gmail.com>
2025-03-30 16:33:43 -0400

    39.User routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,25 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 const port = process.env.PORT || 5000;
+
 connectDB();
+
 const app = express();
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
-  res.send("API is running....");
+  res.send("API is running...");
 });
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
